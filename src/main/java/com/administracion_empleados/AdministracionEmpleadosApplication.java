8d5ca29ba35e4cc5bae13ce794813708ce675b32
A-Albaez <72364146+A-Albaez@users.noreply.github.com>
2023-03-25 19:21:35 -0500

    correcciones

--- src/main/java/com/administracion_empleados/AdministracionEmpleadosApplication.java
+++ src/main/java/com/administracion_empleados/AdministracionEmpleadosApplication.java
@@ -1,6 +1,5 @@
 package com.administracion_empleados;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
