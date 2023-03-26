aca2b5f4e79b99a3262179057b9da8f4bb5d75f5
A-Albaez <72364146+A-Albaez@users.noreply.github.com>
2023-03-25 20:08:39 -0500

    correcciones2.0

--- src/main/java/com/administracion_empleados/AdministracionEmpleadosApplication.java
+++ src/main/java/com/administracion_empleados/AdministracionEmpleadosApplication.java
@@ -2,6 +2,10 @@ package com.administracion_empleados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class AdministracionEmpleadosApplication {
@@ -9,5 +13,21 @@ public class AdministracionEmpleadosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdministracionEmpleadosApplication.class, args);
 	}
+
+	@Bean
+    @Profile("dev")
+    public PropertySourcesPlaceholderConfigurer devPropertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new ClassPathResource("application.properties"));
+        return configurer;
+    }
+
+    @Bean
+    @Profile("prod")
+    public PropertySourcesPlaceholderConfigurer prodPropertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new ClassPathResource("application-prod.properties"));
+        return configurer;
+    }
 
 }
